d9f3fc4f3c57ed975dcb06c427de2e770152ca1a
ProgrameDude <mold.demid@gmail.com>
2024-06-08 13:24:17 +0300

    task 2

--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -15,4 +15,20 @@ typedef struct position {
     int colIndex;
 } position;
 
+// размещает в динамической памяти матрицу
+// размером nRows на nCols. Возвращает матрицу.
+matrix getMemMatrix(int nRows, int nCols);
+
+// размещает в динамической памяти массив из
+// nMatrices матриц размером nRows на nCols.
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
+
+// освобождает память, выделенную под
+// хранение матрицы m.
+void freeMemMatrix(matrix *m);
+
+// освобождает память, выделенную под
+// хранение массива ms из nMatrices матриц.
+void freeMemMatrices(matrix *ms, int nMatrices);
+
 #endif //LABORATORIESWORKS_MATRIX_H
